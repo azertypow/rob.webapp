c841b1538a662d209a4ea0c432d8320fe46a17c6
azertypow <azertypow@gmail.com>
2024-04-25 18:50:05 +0200

    feat: production api

--- fetchApi/fetchApiConf.ts
+++ fetchApi/fetchApiConf.ts
@@ -1,1 +1,2 @@
-export const apiBaseUrl = 'http://localhost:8000/webapp/api/v1/'
+// export const apiBaseUrl  = 'http://localhost:8000/webapp/api/v1/'
+export const apiBaseUrl     = 'https://admin.robvanleijsen.nl/webapp/api/v1/'
