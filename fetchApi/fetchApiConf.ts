499e88d10818c607d42fabbec2209b0ab9d64539
azertypow <nico@villa1203.ch>
2026-03-24 00:04:39 +0100

    chore: prod api base url

--- fetchApi/fetchApiConf.ts
+++ fetchApi/fetchApiConf.ts
@@ -1,2 +1,3 @@
 // export const apiBaseUrl  = 'http://localhost:8000/webapp/api/v1/'
-export const apiBaseUrl     = 'https://admin.robvanleijsen.nl/webapp/api/v1/'
+// export const apiBaseUrl     = 'https://admin.robvanleijsen.nl/webapp/api/v1/'
+export const apiBaseUrl     = 'https://cms.robvanleijsen.ch/webapp/api/v1/'
