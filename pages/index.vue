54e5d15aada7eb97d3640c265c61c2ae326bee47
azertypow <nico@villa1203.ch>
2026-04-22 12:20:52 +0200

    feat: remove elastic scroll for touch home navigation

--- pages/index.vue
+++ pages/index.vue
@@ -250,7 +250,7 @@ function previousGalleryItem() {
     width: 100%;
     height: 100%;
     user-select: none;
-    touch-action: pan-y;
+    touch-action: none;
 }
 
 .v-index__carousel__nav-to-left {
