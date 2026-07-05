612d5be2796663475a3c6138f556ef1198a7f844
azertypow <nico@villa1203.ch>
2026-07-05 15:51:23 +0200

    feat: change mail encoded by timer

--- utils/decodeObfuscatedHTML.ts
+++ utils/decodeObfuscatedHTML.ts
@@ -3,24 +3,14 @@ export function decodeObfuscatedHTML(
   propsToUpdate: Ref<string>,
 ) {
 
-  let mouseMoved = false
-
   function decodeEncodedBlocks(text: string) {
     return text.replaceAll(/:::decode\s([\s\S]*?):::/g, (fullMatch, encodedText: string) => {
       return `${atob(encodedText)}`
     })
   }
 
-  function onUserGesture() {
-    if (mouseMoved) return
-
-    mouseMoved = true
-    window.setTimeout(() => {
-      propsToUpdate.value = decodeEncodedBlocks(obfuscatedText)
-    }, 500)
-  }
-
-  window.addEventListener('mousemove', onUserGesture)
-  window.addEventListener('touchstart', onUserGesture)
+  window.setTimeout(() => {
+    propsToUpdate.value = decodeEncodedBlocks(obfuscatedText)
+  }, 500)
 }
 
